9ea0ba8c90d3ce546060b3abe46c2c80eaf4d55f
luffylv(吕飞) <luffylv@futunn.com>
2019-04-19 15:48:20 +0800

    chore: init

---
+++ .jest.js
@@ -0,0 +1,45 @@
+const transformIgnorePatterns = [
+  '/dist/',
+  'node_modules/[^/]+?/(?!(es|node_modules)/)', // Ignore modules without es dir
+];
+
+module.exports = {
+  name: 'csrf-token',
+  verbose: true,
+//   setupFiles: [
+//     './tests/setup.js',
+//   ],
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'md',
+  ],
+  modulePathIgnorePatterns: [
+    // '/_site/',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    'node',
+  ],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  collectCoverageFrom: [
+    // 'components/**/*.{ts,tsx}',
+    // '!components/*/style/index.tsx',
+    // '!components/style/index.tsx',
+    // '!components/*/locale/index.tsx',
+    // '!components/*/__tests__/**/type.tsx',
+    // '!components/**/*/interface.{ts,tsx}',
+  ],
+  transformIgnorePatterns,
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
+  },
+};
